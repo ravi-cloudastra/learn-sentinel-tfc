6d1dbe4fffe482666b9897bb8c44932e3f1aaba9
ravi-cloudastra <121858252+ravi-cloudastra@users.noreply.github.com>
2023-03-20 14:47:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,24 +2,24 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+#   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
 
-  owners = ["099720109477"] # Canonical
-}
+#   owners = ["099720109477"] # Canonical
+# }
 
 resource "aws_instance" "ubuntu" {
-  ami                    = data.aws_ami.ubuntu.id
+  ami                    = "ami-0557a15b87f6559cf"
   instance_type          = var.instance_type
 
   tags = {
